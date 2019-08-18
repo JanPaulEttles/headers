ef98a0be1854881f0858da26a7be7317151b89ba
Jan Paul Ettles <janpaulettles@gmail.com>
2019-08-18 18:49:55 +0100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # headers
 node project to bunch inspects headers
+
+
+
+sudo snap install sublime-text --classic
+
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+sudo apt-get install nodejs
+sudo apt install git
+
+node -v
+npm -v
+git --version
+
+git config --list --show-origin
+git config --global user.name "Jan Paul Ettles"
+git config --global user.email janpaulettles@gmail.com
+git config --global core.editor path_to_editor
+
+git config --list
+
+sudo snap install heroku --classic
+heroku login
+
+//create a new repo on github
+
+git clone https://github.com/janpaulettles/headers.git
+cd headers
+
+heroku create
+git push heroku master
+heroku ps:scale web=1
+heroku open
+heroku logs --tail
+
+npm init --yes
+
+
+git add .
+git commit -m "initial"
+git push heroku master
+
+npm install fast-csv
+
+
+
--- headers.js
+++ headers.js
@@ -2,7 +2,8 @@
 const fs = require('fs');
 const path = require('path');
 const csv = require('fast-csv');
-const http = require('http');
+//const http = require('http');
+const request = require('request');
 
 
 fs.createReadStream('test.csv')
@@ -14,18 +15,37 @@ fs.createReadStream('test.csv')
 
 function lookUp(data) {
 
+	const options = {
+		url: data[1],
+		headers: {
+			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_0) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.79 Safari/537.4'
+		}
+	};
+
+	request(options, function (error, response, body) {
+		if (response.statusCode > 300 && response.statusCode < 400 && response.headers.location) {
+			console.log('\t\tJAN STATUS: ' + response.statusCode);
+			console.log('HEADERS: ' + JSON.stringify(response.headers));
+		}
+
+		if (!error && response.statusCode == 200) {
+			//const info = JSON.parse(body);
+			//console.log('\t\tJAN STATUS: ' + response.statusCode);
+			//console.log('HEADERS: ' + JSON.stringify(response.headers));
+		}
+	});
+/*
 	var options = {
 		host: data[1],
 		headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_0) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.79 Safari/537.4' },
 		method: 'GET'
 	};
 
-
 	http.get(options, function (response) {
 		// Detect a redirect
 		console.log('get: ' + data[1]);
 		if (response.statusCode > 300 && response.statusCode < 400 && response.headers.location) {
-			console.log('JAN STATUS: ' + response.statusCode);
+			console.log('\t\tJAN STATUS: ' + response.statusCode);
 			console.log('HEADERS: ' + JSON.stringify(response.headers));
 			}
 		if (response.statusCode == 200) {
@@ -37,4 +57,11 @@ function lookUp(data) {
 		}
 	})
 
+*/
+
+
+
 }
+
+
+
